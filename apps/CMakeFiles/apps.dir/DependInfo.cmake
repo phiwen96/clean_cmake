11ec32e578cc09ae5bdea70ab461a59250fd1cce
phiwen96 <philip.sve@hotmail.com>
2021-02-21 16:23:07 +0100

    a

---
+++ apps/CMakeFiles/apps.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/pw/xcodeProjects/clean_cmake/apps/src/main.cpp" "/Users/pw/xcodeProjects/clean_cmake/apps/CMakeFiles/apps.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "apps/include"
+  "library/include/library"
+  "library/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/pw/xcodeProjects/clean_cmake/library/CMakeFiles/library.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ apps/CMakeFiles/apps.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/apps.dir/src/main.cpp.o"
+  "apps"
+  "apps.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/apps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
